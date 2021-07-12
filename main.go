d520b6b06dd3c3de07a83071e46b4c243024323f
desirel <two_somang@icloud.com>
2021-07-12 12:20:50 +0900

    feat: Go 안에서 switch 문의 사용 이해

--- main.go
+++ main.go
@@ -4,15 +4,13 @@ import "fmt"
 
 func canIDrink(age int) bool {
 
-	/**
-	Go 에서는 if 문 안에 바로 변수를 만들 수 있다. (Variable Expression)
-	if else 절 안에서만 사용한다는 의미이다.
-	그리고 ;(세미콜론) 뒤에 만든 변수를 곧바로 사용할 수 있다.
-	*/
-	if koreanAge := age + 2; koreanAge < 18 {
+	switch koreanAge := age + 2; koreanAge {
+	case 10:
 		return false
+	case 18:
+		return true
 	}
-	return true
+	return false
 }
 
 func main() {
